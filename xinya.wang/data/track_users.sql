8d2310c257bacc24e7e7cb0c94eabf93e04aa363
Xinya-Wang <wesse910508@gmail.com>
2020-10-28 12:45:33 -0700

    m8 create json demo; add data file; create 3 fake data file(track_users, track_locations, track_animals); create connection with myPhpadmin.

---
+++ xinya.wang/data/track_users.sql
@@ -0,0 +1,11 @@
+INSERT INTO track_users VALUES
+(1,'Wilson Blair','user1','user1@gmail.com',md5("pass"),'https://via.placeholder.com/400/960/fff/?text=user1','2020-05-02 03:50:17'),
+(2,'Jillian Dillon','user2','user2@gmail.com',md5("pass"),'https://via.placeholder.com/400/750/fff/?text=user2','2020-01-26 02:13:06'),
+(3,'Gloria Peterson','user3','user3@gmail.com',md5("pass"),'https://via.placeholder.com/400/934/fff/?text=user3','2020-07-20 09:48:50'),
+(4,'Bentley Hogan','user4','user4@gmail.com',md5("pass"),'https://via.placeholder.com/400/901/fff/?text=user4','2020-06-07 04:10:04'),
+(5,'Johnnie Lawrence','user5','user5@gmail.com',md5("pass"),'https://via.placeholder.com/400/944/fff/?text=user5','2020-01-15 11:57:01'),
+(6,'Frederick Cole','user6','user6@gmail.com',md5("pass"),'https://via.placeholder.com/400/754/fff/?text=user6','2020-10-01 12:57:14'),
+(7,'Bryan Fisher','user7','user7@gmail.com',md5("pass"),'https://via.placeholder.com/400/844/fff/?text=user7','2020-02-20 11:33:27'),
+(8,'Kenya Tyler','user8','user8@gmail.com',md5("pass"),'https://via.placeholder.com/400/779/fff/?text=user8','2020-06-23 02:08:02'),
+(9,'Penelope Willis','user9','user9@gmail.com',md5("pass"),'https://via.placeholder.com/400/959/fff/?text=user9','2020-06-16 09:48:04'),
+(10,'Lynda Cochran','user10','user10@gmail.com',md5("pass"),'https://via.placeholder.com/400/953/fff/?text=user10','2020-10-17 03:51:34');
